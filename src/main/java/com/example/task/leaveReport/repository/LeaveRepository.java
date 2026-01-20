3af92641d4d7cb571105c67ffd43116307835de3
Shobhit jain <shobhitj484@gmail.com>
2026-01-20 11:11:31 +0530

    Interface of Leave Repository

---
+++ src/main/java/com/example/task/leaveReport/repository/LeaveRepository.java
@@ -0,0 +1,12 @@
+package com.example.task.leaveReport.repository;
+
+import com.example.task.leaveReport.entity.Leave;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface LeaveRepository extends JpaRepository<Leave, Long> {
+
+    Optional<Leave> findByLeaveName(String leaveName);
+
+}
