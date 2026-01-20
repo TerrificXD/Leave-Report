4cad874ff312f838a803b7ca6b9f5be5c6d76c73
Shobhit jain <shobhitj484@gmail.com>
2026-01-20 11:08:04 +0530

    Implementation of Leave DTO

---
+++ src/main/java/com/example/task/leaveReport/dto/LeaveDto.java
@@ -0,0 +1,35 @@
+package com.example.task.leaveReport.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class LeaveDto {
+
+    private Long id;
+
+    @NotBlank(message = "Leave name is required")
+    private String leaveName;
+
+    public LeaveDto() {
+    }
+
+    public LeaveDto(Long id, String leaveName) {
+        this.id = id;
+        this.leaveName = leaveName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLeaveName() {
+        return leaveName;
+    }
+
+    public void setLeaveName(String leaveName) {
+        this.leaveName = leaveName;
+    }
+}
