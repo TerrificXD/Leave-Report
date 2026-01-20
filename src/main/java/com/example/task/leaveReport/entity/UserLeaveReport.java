2516bc93ebf4a4254e05f727823ca9b81c1e776e
Shobhit jain <shobhitj484@gmail.com>
2026-01-20 11:15:29 +0530

    Implementation of User Leave Report Class

---
+++ src/main/java/com/example/task/leaveReport/entity/UserLeaveReport.java
@@ -0,0 +1,103 @@
+package com.example.task.leaveReport.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "user_leave_reports")
+public class UserLeaveReport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "leave_id", nullable = false)
+    private Leave leave;
+
+    @Column(name = "apply_date", nullable = false)
+    private LocalDate applyDate;
+
+    @Column(name = "from_date", nullable = false)
+    private LocalDate fromDate;
+
+    @Column(name = "to_date", nullable = false)
+    private LocalDate toDate;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    public UserLeaveReport() {
+    }
+
+    public UserLeaveReport(User user, LocalDate toDate, Leave leave, LocalDate fromDate, LocalDate applyDate, String description) {
+        this.user = user;
+        this.toDate = toDate;
+        this.leave = leave;
+        this.fromDate = fromDate;
+        this.applyDate = applyDate;
+        this.description = description;
+    }
+
+    public LocalDate getApplyDate() {
+        return applyDate;
+    }
+
+    public void setApplyDate(LocalDate applyDate) {
+        this.applyDate = applyDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(LocalDate fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Leave getLeave() {
+        return leave;
+    }
+
+    public void setLeave(Leave leave) {
+        this.leave = leave;
+    }
+
+    public LocalDate getToDate() {
+        return toDate;
+    }
+
+    public void setToDate(LocalDate toDate) {
+        this.toDate = toDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
