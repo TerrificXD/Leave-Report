fa54559a41913a7f665858c850857d98192c25a9
Shobhit jain <shobhitj484@gmail.com>
2026-01-20 11:17:14 +0530

    main method

---
+++ src/test/java/com/example/task/leaveReport/LeaveReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task.leaveReport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
