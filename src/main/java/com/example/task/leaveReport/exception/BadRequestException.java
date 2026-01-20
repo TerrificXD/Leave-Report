8fa8707267dc1923c88b479bfacc66b8d072b867
Shobhit jain <shobhitj484@gmail.com>
2026-01-20 11:26:04 +0530

    Bad Request Exception class

--- src/main/java/com/example/task/leaveReport/exception/BadRequestException.java
+++ src/main/java/com/example/task/leaveReport/exception/BadRequestException.java
@@ -1,5 +1,6 @@
 package com.example.task.leaveReport.exception;
 
+
 public class BadRequestException extends RuntimeException {
     public BadRequestException(String message) {
         super(message);
