497383aaf2265c3553f257f9a0e38149872cb360
Shobhit jain <shobhitj484@gmail.com>
2026-01-20 11:28:27 +0530

    Leave Repository interface

--- src/main/java/com/example/task/leaveReport/repository/LeaveRepository.java
+++ src/main/java/com/example/task/leaveReport/repository/LeaveRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+
 public interface LeaveRepository extends JpaRepository<Leave, Long> {
 
     Optional<Leave> findByLeaveName(String leaveName);
