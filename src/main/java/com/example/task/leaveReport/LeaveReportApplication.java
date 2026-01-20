423fce624000883072db8f03e7cff49439a582d4
Shobhit jain <shobhitj484@gmail.com>
2026-01-20 11:08:34 +0530

    Main Method

---
+++ src/main/java/com/example/task/leaveReport/LeaveReportApplication.java
@@ -0,0 +1,13 @@
+package com.example.task.leaveReport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveReportApplication.class, args);
+	}
+
+}
